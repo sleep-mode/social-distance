25b4894b0f3dc84fabbe69c9ccecc6d33b86d188
MINJAE <pmj933@naver.com>
2021-05-22 15:17:53 +0900

    Add api.ts

---
+++ client/src/game/utils/api.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+/* Example
+import { postScore, getHighScores } from './game/utils/api';
+
+postScore({
+  id: "test1234",
+  date: "2021-05-22",
+  nickname: name,
+  message: "1등할거다",
+  score: Math.floor(Math.random() * 1000)
+});
+getHighScores();
+*/
+
+interface ScoreData {
+  id: string;
+  nickname: string;
+  score: number;
+  message: string;
+  date: string;
+}
+
+const reqApi = axios.create({
+  baseURL: 'https://gylmaxhi6e.execute-api.ap-northeast-2.amazonaws.com/',
+  timeout: 5000
+});
+
+export async function getHighScores () { // get Top 5
+  let req = await reqApi.get(`/scores`);
+  return req.data.Items;
+}
+
+export async function postScore (score: ScoreData) {
+  let req = await reqApi.post(`/scores`, {
+    id: score.id,
+    date: new Date().toISOString().substring(0, 10),
+    name: score.nickname,
+    msg: score.message,
+    score: score.score
+  });
+  return req;
+}
+
